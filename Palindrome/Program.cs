b5ff86ef01081aec615482a3811e05e9671383b1
Furkan TUNÇ <furkant88@outlook.com>
2021-11-15 12:41:26 +0300

    Verilen cümledeki palindrome elimeler bulundu.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cumle = "bu gün hava çok güzel....arkadaşlar ile kavak olan bir yere gittik. tabi gitmeden ütü yaptım. orada ata bindim....";
+
+            string[] cumleler = cumle.Split(' ');
+
+            foreach (var item in cumleler)
+            {
+                int i = 0;
+                while (i < item.Length)
+                {
+                    if (item[i] == item[item.Length - (i + 1)])
+                    {
+                        i++; 
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (i==item.Length)
+                {
+                    Console.WriteLine($"{item} bir palindromedir.");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
